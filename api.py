074f045f98db45fdfa6b205895db685093e25163
Ashley Evans <ashley.evans@md.catapult.org.uk>
2021-08-23 09:58:30 +0100

    implemented email simple message

--- api.py
+++ api.py
@@ -4,6 +4,13 @@ app = Flask(__name__)
 api = Api(app)
 from gene_of_interest import poi_proteome, proteome_comparison
 import sys
+import smtplib, ssl
+from email.message import EmailMessage
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 # user_put_args = reqparse.RequestParser()
@@ -16,9 +23,26 @@ import sys
 #     if question_id not in questions:
 #         abort(404, message="Question id is not valid.")
 
+def send_mail(send_from, send_to, subject, text, file=None, server="localhost"):
+    email = "ashevans3000@gmail.com"
+    password = "test3000"
+    reciever_email = "ashevans3@gmail.com"
+    print(email)
+    msg=EmailMessage()
+    msg['Subject'] = 'AR'
+    msg['From'] = email
+    msg['To'] = reciever_email
+    msg.set_content('Ok it works')
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email, password)
+        print("Logged in")
+        smtp.send_message(msg)
+        print("email sent")
+
 def sys_out(gene_choice: str, organism_choice: str, list_in_common: list):
     original_stout = sys.stdout
-    with open(gene_choice + "_" + organism_choice, "w") as f:
+    file_name: str = gene_choice + "_" + organism_choice
+    with open(file_name, "w") as f:
         sys.stdout = f
         print("1. Gene name.")
         print("2. Number of proteome hits in common.")
@@ -39,20 +63,23 @@ def sys_out(gene_choice: str, organism_choice: str, list_in_common: list):
             print("\n")
         sys.stdout = original_stout
         print("Complete! Check the files.")
+        return file_name
 
 class Proteome(Resource):
 
-    def put(self, gene_name, species_id):
+    def put(self, gene_name, species_id, email):
         # args = user_put_args.parse_args()
         print(gene_name)
         print(species_id)
+        print(email)
         proteome = poi_proteome(gene_name, species_id)
         list_in_common = proteome_comparison(proteome, species_id)
-        sys_out(gene_name, species_id, list_in_common)
+        file_name = sys_out(gene_name, species_id, list_in_common)
+        send_mail(email, email, "results", "This has worked")
 
 
 
-api.add_resource(Proteome, "/<string:gene_name>/<string:species_id>")
+api.add_resource(Proteome, "/<string:gene_name>/<string:species_id>/<string:email>")
 
 if __name__ == "__main__":
     app.run(debug=True)
